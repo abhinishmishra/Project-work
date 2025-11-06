e325bcf95535983faf70c1eff8ed593fa6707202
Abhinish Mishra <abhanishm@gmail.com>
2025-11-06 17:58:15 +0530

    Add files via upload

---
+++ StringFunc_02.py
@@ -0,0 +1,47 @@
+s = "Banana"
+print(s.find("a"))
+print(s.count("a"))
+print(s.replace("a","o"))
+
+#methods
+print("length of s is:",len(s))
+print("length of s is:",min(s))
+print("length of s is:",max(s))
+
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.capitalize())
+print(s.title())
+
+
+print(s.endswith("b"))
+print(s.startswith("b"))
+
+
+data = "a,b,c"
+print(data.split(","))  # ['a','b','c']
+
+words = ['Hello', 'Python']
+print(" ".join(words))  # Hello Python
+
+'''scape	Meaning
+\n	New line
+\t	Tab space
+\'	Single quote
+\"	Double quote
+\\	Backslash'''
+
+print("Hello\nWorld")
+print("Hello\tWorld")
+print("Hello\'World")
+print("Hello\\World")
+
+#f-string (Best & Modern)
+name = "Abhinish"
+age = 21
+print(f"My name is {name} and I am {age} years old")
+
+print("Hello {}, your age is {}".format(name, age))
+
+
