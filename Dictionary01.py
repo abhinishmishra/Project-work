c54647df70afdcbcbf3265c35bfa05f42f4842d5
Abhinish Mishra <abhanishm@gmail.com>
2025-11-11 18:10:14 +0530

    Add files via upload

---
+++ Dictionary01.py
@@ -0,0 +1,21 @@
+'''📌 What is Dictionary?
+
+Dictionary is a key-value pair data structure.
+
+Ordered (Python 3.7+)
+
+Mutable (changeable)
+
+Keys must be unique
+
+Values can be duplicate
+
+Keys can be only immutable types (str, int, tuple)'''
+
+my_dict = {
+    "name": "Abhinish",
+    "age": 21,
+    "city": "Delhi"
+}
+print(my_dict["name"])
+print(my_dict.get("age"))
